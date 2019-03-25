28f26378a2ec05b0b8a57bafee2b055c6334d838
Peter Choi <ptrchoi@gmail.com>
2019-03-25 10:19:28 -0500

    added listener to match parent width from 'fixed' child position instead of viewport

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,38 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import $ from 'jquery';
 
 import Title from '../Title/Title';
 import Sidebar from '../Sidebar/Sidebar';
 import MenuIcon from '../MenuIcon/MenuIcon';
 
-const Navbar = props => {
-  const { mq } = props;
-  console.log('mq: ', mq);
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
 
-  const toggleSidebarHandler = () => {
-    console.log('toggleSidebarHandler');
-    // $('#icon-wrapper').toggleClass('sidebar-icon--close');
-    $('#navSidebar').toggleClass('navSidebar--show');
-  };
-
-  if (mq === 'wide') {
-    return (
-      <div className="navbar-wrapper navbar-wrapper--wide">
-        <Title />
-        <div>{props.links}</div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="navbar-wrapper navbar-wrapper--narrow">
-        <Title />
-        <MenuIcon toggleSidebar={toggleSidebarHandler} />
-        <div id="navSidebar" className="navSidebar--hide">
-          <Sidebar links={props.links} />
-        </div>
-      </div>
-    );
+    this.matchParentWidth = this.matchParentWidth.bind(this);
   }
-};
+  componentDidMount() {
+    window.addEventListener('resize', this.matchParentWidth);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.matchParentWidth);
+  }
+  matchParentWidth() {
+    let parentWidth = $('.parent').width();
+    $('.child').width(parentWidth);
+  }
+  render() {
+    const { mq } = this.props;
+    console.log('mq: ', mq);
+
+    const toggleSidebarHandler = () => {
+      console.log('toggleSidebarHandler');
+      $('#navSidebar').toggleClass('navSidebar--hide');
+    };
+
+    if (mq === 'wide') {
+      return (
+        <div className="navbar-wrapper navbar-wrapper--wide">
+          <Title />
+          <div>{this.props.links}</div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="navbar-wrapper navbar-wrapper--narrow">
+          <Title />
+          <MenuIcon toggleSidebar={toggleSidebarHandler} />
+          <div id="navSidebar" className="navSidebar--hide">
+            <Sidebar links={this.props.links} />
+          </div>
+        </div>
+      );
+    }
+  }
+}
 
 export default Navbar;
