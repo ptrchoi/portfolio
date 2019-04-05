da2d9b5d3daef47a9715569d1b68c122edbb1163
Peter Choi <ptrchoi@gmail.com>
2019-04-04 22:12:03 -0500

    first pass implementation of no menu on landing page; fixed windowScroll calc

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -12,6 +12,9 @@ import Home from '../Home/Home';
 import About from '../About/About';
 import Skills from '../Skills/Skills';
 
+//TEMP UNTIL COMPONENT HEIGHTS ARE SET
+const ABOUT_SECTION_HEIGHT = 800;
+
 /**
  * App Component handles:
  * - routing
@@ -24,59 +27,96 @@ class App extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      landingPageHeight: 800,
+      onLandingPage: true
+    };
+
     this.updateNavOnScroll = this.updateNavOnScroll.bind(this);
   }
   componentDidMount() {
     window.addEventListener('scroll', this.updateNavOnScroll);
+
+    //Set the size of the Landing Page
+    let height = window.innerHeight;
+    $(window).resize(function() {
+      $('#home-section').css('height', height);
+    });
+    this.setState({
+      landingPageHeight: height
+    });
   }
   componentWillUnmount() {
     window.removeEventListener('scroll', this.updateNavOnScroll);
   }
   updateNavOnScroll() {
+    let { landingPageHeight, onLandingPage } = this.state;
     const winScroll =
       document.body.scrollTop || document.documentElement.scrollTop;
-    const height =
-      document.documentElement.scrollHeight -
-      document.documentElement.clientHeight;
-    const scrolled = winScroll / height;
 
     $('.menuLink').removeClass('activeLink');
 
-    // Check page scroll location and update activeLink as needed
-    if (scrolled < C.ABOUT_TOP - C.SCROLL_TOP_OFFSET) {
+    // Check page scroll location/height and update activeLink & onLandingPage as needed
+    if (winScroll < landingPageHeight) {
       $('#menu-home').toggleClass('activeLink');
-    } else if (scrolled < C.SKILLS_TOP - C.SCROLL_TOP_OFFSET) {
+      onLandingPage = true;
+    } else if (winScroll < landingPageHeight + ABOUT_SECTION_HEIGHT) {
       $('#menu-about').toggleClass('activeLink');
+      onLandingPage = false;
     } else {
       $('#menu-skills').toggleClass('activeLink');
+      onLandingPage = false;
     }
+    this.setState({
+      onLandingPage: onLandingPage
+    });
   }
   render() {
-    return (
-      <div className="app-wrapper">
-        <BrowserRouter>
-          <div>
-            <MediaQuery query="(min-device-width: 1224px)">
-              <Navbar mq={'wide'} links={<Links size={'wide'} />} />
-            </MediaQuery>
-            <MediaQuery query="(max-device-width: 1224px)">
-              <Navbar mq={'narrow'} links={<Links size={'narrow'} />} />
-            </MediaQuery>
-          </div>
-        </BrowserRouter>
-        <div className="content-wrapper">
-          <div id="home">
-            <Home />
-          </div>
-          <div id="about">
-            <About />
-          </div>
-          <div id="skills">
-            <Skills />
+    const { onLandingPage } = this.state;
+
+    if (onLandingPage) {
+      return (
+        <div className="app-wrapper">
+          <div className="content-wrapper">
+            <div id="home">
+              <Home height={this.state.landingPageHeight} />
+            </div>
+            <div id="about">
+              <About />
+            </div>
+            <div id="skills">
+              <Skills />
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className="app-wrapper">
+          <BrowserRouter>
+            <div>
+              <MediaQuery query="(min-device-width: 1224px)">
+                <Navbar mq={'wide'} links={<Links size={'wide'} />} />
+              </MediaQuery>
+              <MediaQuery query="(max-device-width: 1224px)">
+                <Navbar mq={'narrow'} links={<Links size={'narrow'} />} />
+              </MediaQuery>
+            </div>
+          </BrowserRouter>
+          <div className="content-wrapper">
+            <div id="home">
+              <Home />
+            </div>
+            <div id="about">
+              <About />
+            </div>
+            <div id="skills">
+              <Skills />
+            </div>
+          </div>
+        </div>
+      );
+    }
   }
 }
 
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import $ from 'jquery';
+
+const IMAGE_TRANSITION_TIME = 0.8; //s
 
 /**
- * Functional React component, simply renders a page content element.
+ * React Class Component, renders a page content element and fullpage background image.
  * @function
- * @return {JSX.Element} - Rendered component.
+ * @return {JSX.Element} - Rendered component, with href link button.
  */
-const Home = () => {
-  return (
-    <div className="sectionWrapper">
-      <p>Home Component</p>
-      <button
-        className="welcomeButton"
-        onClick={() => console.log('welcome button pressed!')}
+class Home extends Component {
+  componentDidMount() {
+    let url =
+      'https://source.unsplash.com/user/markusspiske/jUWw_NEXjDw/1600x1200';
+    let imageProperties = {
+      backgroundImage: `url(${url})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center center',
+      backgroundAttachment: 'fixed',
+      transition: `all ease-in-out ${IMAGE_TRANSITION_TIME}s`
+    };
+
+    let el = document.getElementById('home-section');
+    Object.assign(el.style, imageProperties);
+  }
+  render(props) {
+    let { height } = this.props;
+    console.log('props passed in height: ', height);
+    return (
+      <div
+        id="home-section"
+        className="home-wrapper"
+        style={{ height: height }}
       >
-        welcome
-      </button>
-    </div>
-  );
-};
+        <p>Home Component</p>
+
+        <button
+          className="welcomeButton"
+          onClick={() => (window.location.href = '/#about')}
+        >
+          welcome
+        </button>
+      </div>
+    );
+  }
+}
 
 export default Home;
