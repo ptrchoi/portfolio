3687945d00457090194ffde3d25a421bf8245fb9
Peter Choi <ptrchoi@gmail.com>
2019-07-08 15:41:07 -0500

    added rotating card animation functionality to portfolio section

--- src/Components/Portfolio/portfolio.jsx
+++ src/Components/Portfolio/portfolio.jsx
@@ -1,76 +1,124 @@
 //Libraries
-import React from 'react';
+import React, { Component } from 'react';
+import $ from 'jquery';
 
 /**
  * Functional React Component, renders a content section of "cards".
  * @function
  * @return {JSX.Element} - Rendered component.
  */
-const Portfolio = props => {
-  return (
-    <div className="section-wrapper portfolio-wrapper">
-      <div className="project-card">
-        <img
-          src={'https://codepen.io/ptrchoi/pen/JayyOP/image/large.png'}
-          className="project-image"
-          alt="thumbnail"
-        />
-        <div className="project-text">
-          <p>Product Landing Page</p>
+class Portfolio extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      active: false
+    };
+
+    this.toggleClass = this.toggleClass.bind(this);
+  }
+  toggleClass() {
+    const currentState = this.state.active;
+    this.setState({ active: !currentState });
+  }
+  render() {
+    return (
+      <div className="section-wrapper portfolio-wrapper">
+        {/* Product Landing Page Card */}
+        <div className="card">
+          <div
+            id="project-1"
+            className={
+              this.state.active ? 'card-content card-flipped' : 'card-content'
+            }
+            onClick={this.toggleClass}
+          >
+            <div className="card-content--front">
+              <img
+                src={'https://codepen.io/ptrchoi/pen/JayyOP/image/large.png'}
+                alt="thumbnail"
+              />
+              <div>
+                <p>Product Landing Page</p>
+              </div>
+            </div>
+            <div className="card-content--back">
+              <a href="https://codepen.io/ptrchoi/pen/JayyOP" target="_blank">
+                <p>BACK of the card text</p>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        {/* Product Landing Page Card */}
+        <div className="card">
+          <div
+            id="project-2"
+            className={
+              this.state.active ? 'card-content card-flipped' : 'card-content'
+            }
+            onClick={this.toggleClass}
+          >
+            <div className="card-content--front">
+              <img
+                src={'https://codepen.io/ptrchoi/pen/QJRpvQ/image/large.png'}
+                alt="thumbnail"
+              />
+              <div>
+                <p>Calculator App</p>
+              </div>
+            </div>
+            <div className="card-content--back">
+              <a href="https://codepen.io/ptrchoi/pen/JayyOP" target="_blank">
+                <p>BACK of the card text</p>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="card">
+          <img
+            src={'https://codepen.io/ptrchoi/pen/Xyrppy/image/large.png'}
+            className="front-image"
+            alt="thumbnail"
+          />
+          <div className="front-title">
+            <p>Drum Machine App</p>
+          </div>
+        </div>
+        <div className="card">
+          <img
+            src={'https://codepen.io/ptrchoi/pen/mQEMXv/image/large.png'}
+            className="front-image"
+            alt="thumbnail"
+          />
+          <div className="front-title">
+            <p>Random Quote Generator</p>
+          </div>
+        </div>
+        <div className="card">
+          <img
+            src={'https://codepen.io/ptrchoi/pen/jvLoOQ/image/large.png'}
+            className="front-image"
+            alt="thumbnail"
+          />
+          <div className="front-title">
+            <p>Technical Documentation</p>
+          </div>
+        </div>
+        <div className="card">
+          <img
+            src={'https://codepen.io/ptrchoi/pen/rqPKJV/image/large.png'}
+            className="front-image"
+            alt="thumbnail"
+          />
+          <div className="front-title">
+            <p>Markdown Live Previewer</p>
+          </div>
         </div>
       </div>
-      <div className="project-card">
-        <img
-          src={'https://codepen.io/ptrchoi/pen/QJRpvQ/image/large.png'}
-          className="project-image"
-          alt="thumbnail"
-        />
-        <div className="project-text">
-          <p>Calculator App</p>
-        </div>
-      </div>
-      <div className="project-card">
-        <img
-          src={'https://codepen.io/ptrchoi/pen/Xyrppy/image/large.png'}
-          className="project-image"
-          alt="thumbnail"
-        />
-        <div className="project-text">
-          <p>Drum Machine App</p>
-        </div>
-      </div>
-      <div className="project-card">
-        <img
-          src={'https://codepen.io/ptrchoi/pen/mQEMXv/image/large.png'}
-          className="project-image"
-          alt="thumbnail"
-        />
-        <div className="project-text">
-          <p>Random Quote Generator</p>
-        </div>
-      </div>
-      <div className="project-card">
-        <img
-          src={'https://codepen.io/ptrchoi/pen/jvLoOQ/image/large.png'}
-          className="project-image"
-          alt="thumbnail"
-        />
-        <div className="project-text">
-          <p>Technical Documentation</p>
-        </div>
-      </div>
-      <div className="project-card">
-        <img
-          src={'https://codepen.io/ptrchoi/pen/rqPKJV/image/large.png'}
-          className="project-image"
-          alt="thumbnail"
-        />
-        <div className="project-text">
-          <p>Markdown Live Previewer</p>
-        </div>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Portfolio;
