ac5deba47461e1f93bcfb5d9bf4b131d384d411a
Peter Choi <ptrchoi@gmail.com>
2020-01-20 20:30:57 -0600

    updated Portfolio Cards with more projects and associated screenshots added to Assets

--- src/Components/Portfolio/portfolio.jsx
+++ src/Components/Portfolio/portfolio.jsx
@@ -6,10 +6,18 @@ import Card from '../card/Card';
 import Confetti from '../confetti/Confetti';
 
 //Assets
-import screenshot1 from '../../assets/project_1.png';
-import screenshot2 from '../../assets/project_2.png';
-import screenshot3 from '../../assets/project_3.png';
-import screenshot4 from '../../assets/project_4.png';
+import screenSpa from '../../assets/project_spa.png';
+import screenCalc from '../../assets/project_calc.png';
+import screenTech from '../../assets/project_tech.png';
+import screenDrum from '../../assets/project_drum.png';
+import screenQuote from '../../assets/project_quote.png';
+import screenSimon from '../../assets/project_simon.png';
+import screenTictac from '../../assets/project_tictac.png';
+import screenMarkdown from '../../assets/project_markdown.png';
+import screenTwitch from '../../assets/project_twitch.png';
+import screenPomodoro from '../../assets/project_pomodoro.png';
+import screenWeather from '../../assets/project_weather.png';
+import screenWiki from '../../assets/project_wiki.png';
 
 /**
  * Functional React Component, renders a content section of "cards".
@@ -21,53 +29,105 @@ const CARDS = [
 	{
 		projectId: 'project-1',
 		title: 'Single-page Application (SPA) - Product Page',
-		image: screenshot1,
+		image: screenSpa,
 		link: 'https://codepen.io/ptrchoi/pen/JayyOP',
 		description:
-			'A pure JS & CSS demo of a single-page application, responsive product page. With nested Flex-Box & CSS Grid layouts, nav-menu & image gallery modals, and user-centered design elements throughout.',
+			'A pure JS & CSS demo of a single-page application (SPA), fully responsive product page. Nested Flex-Box & CSS Grid layouts, scaling video background, nav-menu & image gallery modals, and user-centered design elements throughout.',
 		flipped: false
 	},
 	{
 		projectId: 'project-2',
-		title: 'REACT Calculator',
-		image: screenshot4,
-		link: 'https://codepen.io/ptrchoi/pen/QJRpvQ',
+		title: 'REACT Drum Machine',
+		image: screenDrum,
+		link: 'https://codepen.io/ptrchoi/pen/Xyrppy',
 		description:
-			'A simple REACT Calculator Web App with a clean, modern Material theme, demonstrating functional programming with higher-order functions, advanced ES6 features, and modular programming with React and SCSS components.',
+			'A REACT Drum Machine app, responsive with touch and keyboard inputs, demonstrating functional programming  with higher-order functions, advanced ES6 features, and modular programming with React and SCSS components.',
 		flipped: false
 	},
 	{
 		projectId: 'project-3',
-		title: 'REACT Drum Machine',
-		image: screenshot2,
-		link: 'https://codepen.io/ptrchoi/pen/Xyrppy',
-		description: 'Click to check it out on Codepen',
+		title: 'Weather App',
+		image: screenWeather,
+		link: 'https://codepen.io/ptrchoi/pen/grwMJv',
+		description: 'Weather App',
 		flipped: false
 	},
 	{
 		projectId: 'project-4',
-		title: 'Technical Documentation Page',
-		image: screenshot3,
-		link: 'https://codepen.io/ptrchoi/pen/jvLoOQ',
-		description:
-			"A pure JS & CSS demo of a responsive technical document, recreating a page from VS Code's site from scratch. With active menu scrolling, persistent modal nav-menu, and modern Code Element styling.",
+		title: 'Pomodoro Clock App',
+		image: screenPomodoro,
+		link: 'https://codepen.io/ptrchoi/pen/RaQdya',
+		description: 'POMODORO CLOCK APP',
 		flipped: false
 	},
 	{
 		projectId: 'project-5',
 		title: 'Markdown Editor with Live Preview',
-		image: screenshot1,
+		image: screenMarkdown,
 		link: 'https://codepen.io/ptrchoi/pen/rqPKJV',
-		description: 'Click to check it out on Codepen',
+		description:
+			'A Live Markdown Editor Preview App, built in REACT. Adhering to GIT Markdown rules, the Editor window displays the resulting Markdown in real-time.',
 		flipped: false
 	},
 	{
 		projectId: 'project-6',
+		title: 'REACT Calculator',
+		image: screenCalc,
+		link: 'https://codepen.io/ptrchoi/pen/QJRpvQ',
+		description:
+			'A simple REACT Calculator app with a clean, modern Material theme, responsive with touch and keyboard inputs, demonstrating functional programming with higher-order functions, advanced ES6 features, and modular programming with React and SCSS components.',
+		flipped: false
+	},
+	{
+		projectId: 'project-7',
 		title: 'Random Quote Generator',
-		image: screenshot4,
+		image: screenQuote,
 		link: 'https://codepen.io/ptrchoi/pen/mQEMXv',
 		description:
 			'A simple REACT Web App which generates randomized quotes and backgrounds, integrating with the Forismatic API. Fully responsive and designed to have a calming effect.',
+		flipped: false
+	},
+	{
+		projectId: 'project-8',
+		title: 'Simon Game',
+		image: screenSimon,
+		link: 'https://codepen.io/ptrchoi/pen/wGXNKm',
+		description:
+			'The classic game of Simon, app built with pure JS & CSS. Features custom assets, animations, audio, Start and Strict game features.',
+		flipped: false
+	},
+	{
+		projectId: 'project-9',
+		title: 'Wikipedia Reader App',
+		image: screenWiki,
+		link: 'https://codepen.io/ptrchoi/pen/ZWLMzq',
+		description: 'Wikipedia Reader App',
+		flipped: false
+	},
+	{
+		projectId: 'project-10',
+		title: 'Tic-Tac-Toe Game',
+		image: screenTictac,
+		link: 'https://codepen.io/ptrchoi/pen/zqjzaL',
+		description:
+			'A classic Tic-Tac-Toe game app built with pure JS & CSS. Features Game AI with text feedback on two difficulty level settings, score tracking, piece selection, and reset.',
+		flipped: false
+	},
+	{
+		projectId: 'project-11',
+		title: 'Twitch TV App',
+		image: screenTwitch,
+		link: 'https://codepen.io/ptrchoi/pen/JXWOdL',
+		description: 'Twitch TV App',
+		flipped: false
+	},
+	{
+		projectId: 'project-12',
+		title: 'Technical Documentation Page',
+		image: screenTech,
+		link: 'https://codepen.io/ptrchoi/pen/jvLoOQ',
+		description:
+			"A pure JS & CSS demo of a responsive technical document, recreating a page from VS Code's site from scratch. Featuring active menu scrolling, persistent modal nav-menu, and modern Code Element styling.",
 		flipped: false
 	}
 ];
@@ -98,6 +158,12 @@ class Portfolio extends Component {
 					<Card card={card[3]} />
 					<Card card={card[4]} />
 					<Card card={card[5]} />
+					<Card card={card[6]} />
+					<Card card={card[7]} />
+					<Card card={card[8]} />
+					<Card card={card[9]} />
+					<Card card={card[10]} />
+					<Card card={card[11]} />
 					<div className="confetti blur">
 						<Confetti
 							data={{
