8feb421210e70e0c031dd44dc1e9a96e10bf0364
Peter Choi <ptrchoi@gmail.com>
2020-10-12 08:19:45 -0500

    added missing/updated firebase firestore config to fix bugs

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,18 +1,20 @@
 import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 // App's Firebase configuration (apiKey is domain restricted/secure)
 const firebaseConfig = {
-	apiKey: '***REMOVED***',
-	authDomain: '***REMOVED***',
-	databaseURL: '***REMOVED***',
-	projectId: 'ptrchoi-portfolio',
-	storageBucket: '***REMOVED***',
-	messagingSenderId: '***REMOVED***',
-	appId: '1:***REMOVED***:web:***REMOVED***',
-	measurementId: '***REMOVED***'
+  apiKey: '***REMOVED***',
+  authDomain: '***REMOVED***',
+  databaseURL: '***REMOVED***',
+  projectId: 'ptrchoi-portfolio',
+  storageBucket: '***REMOVED***',
+  messagingSenderId: '***REMOVED***',
+  appId: '1:***REMOVED***:web:***REMOVED***',
+  measurementId: '***REMOVED***',
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+let firestore = firebase.firestore();
 
 export default firebase;
